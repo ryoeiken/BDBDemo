7ed7b335fe888e352491b1129869afd4c76d5d31
ryoeiken <j.von_lucian@hotmail.com>
2018-08-25 17:39:39 +0800

    动态导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,13 +4,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    swiperImgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
-
-    imgUrls:'',
+    imgUrls: '',
 
     indicatorDots: true,
     autoplay: true,
@@ -18,43 +12,7 @@ Page({
     duration: 1000,
     circular: true,
 
-    nav: [{
-        url: 'https://i.jpg.dog/img/b6879f193eaf82b327f3deaede2c8114.png',
-        name: '美食'
-      },
-      {
-        url: 'https://i.jpg.dog/img/37b2e7c602b1a089e3e78a568e22dffa.png',
-        name: '洗浴足疗'
-      },
-      {
-        url: 'https://i.jpg.dog/img/9e343254d3017e944bfa6c783151957f.png',
-        name: '结婚啦'
-      },
-      {
-        url: 'https://i.jpg.dog/img/50ed787c3debe35eebc9151115cc1b41.png',
-        name: '卡拉OK'
-      },
-      {
-        url: 'https://i.jpg.dog/img/098e16d29e0c24c97568e3c1b80c3bf7.png',
-        name: '找工作'
-      },
-      {
-        url: 'https://i.jpg.dog/img/803165c1c046d1f85630933b927dd3a2.png',
-        name: '辅导班'
-      },
-      {
-        url: 'https://i.jpg.dog/img/03f32489840564425078da272e4a44e7.png',
-        name: '汽车保养'
-      },
-      {
-        url: 'https://i.jpg.dog/img/57322b0d201f8e84b4088ed829616353.png',
-        name: '租房'
-      },
-      {
-        url: 'https://i.jpg.dog/img/95de172614f537c36df654bb18ac616d.png',
-        name: '装修'
-      }
-    ],
+    navUrls: '',
 
     linksImgUrls: [
       'https://i.jpg.dog/img/aba6bc7b706004625a04c5009e429939.png',
@@ -66,13 +24,24 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     wx.request({
       url: 'https://locally.uieee.com/slides',
-      method:'GET',
-      success:(res) =>{
+      method: 'GET',
+      success: (res) => {
         this.setData({
-          imgUrls:res.data
+          imgUrls: res.data
+        })
+      }
+    })
+
+    wx.request({
+      url: 'https://locally.uieee.com/categories',
+      method: 'GET',
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          navUrls: res.data
         })
       }
     })
@@ -82,49 +51,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
