92eb9d5a0e825e551b0ec06db81394fa41165244
ryoeiken <j.von_lucian@hotmail.com>
2018-08-25 16:05:28 +0800

    完成静态首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,72 +4,94 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
+    swiperImgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
+
     indicatorDots: true,
     autoplay: true,
     interval: 4000,
     duration: 1000,
-    circular: true
+    circular: true,
+
+    navImgUrls: [
+      'https://i.jpg.dog/img/b6879f193eaf82b327f3deaede2c8114.png',
+      'https://i.jpg.dog/img/37b2e7c602b1a089e3e78a568e22dffa.png',
+      'https://i.jpg.dog/img/9e343254d3017e944bfa6c783151957f.png',
+      'https://i.jpg.dog/img/50ed787c3debe35eebc9151115cc1b41.png',
+      'https://i.jpg.dog/img/098e16d29e0c24c97568e3c1b80c3bf7.png',
+      'https://i.jpg.dog/img/803165c1c046d1f85630933b927dd3a2.png',
+      'https://i.jpg.dog/img/03f32489840564425078da272e4a44e7.png',
+      'https://i.jpg.dog/img/57322b0d201f8e84b4088ed829616353.png',
+      'https://i.jpg.dog/img/95de172614f537c36df654bb18ac616d.png'
+    ],
+
+    navItems: [
+      '美食', '洗浴足疗', '结婚啦', '卡拉OK', '找工作', '辅导班', '汽车保养', '租房', '装修'
+    ],
+
+    linksImgUrls:[
+      'https://i.jpg.dog/img/aba6bc7b706004625a04c5009e429939.png',
+      'https://i.jpg.dog/img/028979711a10a3e5a3e78fb7970dbb86.png'
+    ]
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
